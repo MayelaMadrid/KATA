c4c07c64d7b5beffc23bb4b8e8bd49f590f95d26
Mayela Madrid <mayelamgtz@gmail.com>
2021-07-19 22:34:02 -0600

    add audio and animation to start

--- docs/StreetFighter.js
+++ docs/StreetFighter.js
@@ -9,10 +9,19 @@ export default class StreetFighter {
     this.keyBoardManager = new KeyBoard(this.uimanager, defaultIndex);
   }
 
+  playAudio() {
+    let audio = new Audio("./audio/videoplayback.mp4");
+    audio.oncanplaythrough = () => {
+      audio.play();
+    };
+  }
+
   runBoard() {
+    this.playAudio();
     this.keyBoardManager.run();
     this.uimanager.run();
   }
+
   start() {}
   restart() {}
 }
--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,3 @@
 import StreetFighter from "./StreetFighter.js";
 
-const portrait = document.getElementById("portrait");
-
 new StreetFighter().runBoard();
